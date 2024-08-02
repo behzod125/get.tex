364cf52b53781bb7481ae38889d60f9bbc347bf4
Behzod <behzodfarmonqulov@gmail.coom>
2024-08-02 15:49:36 +0500

    new file test

---
+++ main/main.js
@@ -0,0 +1,28 @@
+
+
+// let nami = "Behzodjon";
+// let name = "Farmonqulov";
+// console.log(name = "Behzodjon");
+
+// var number = 8;
+// var number = 25;
+// var number = 1225;
+// var number = 745745;
+// console.log(number);
+
+//  const test = "salom"
+//  const testi = "yaxshimisiz"
+//  console.log(test);
+
+//  let a = "qaliysiz"
+//  var nama = "qayerdansiz"
+//  let t = 49/7;
+//  console.log(t);
+
+
+
+//  let nama = "behzod";
+//  var a = "wel kom"                                                                                                             
+
+
+
